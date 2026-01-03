1ecb708ecfe120d3ded02253d42e10d2e4d93f67
SR01-coder <sam.rawal06@gmail.com>
2026-01-03 15:27:54 +0530

    task files

---
+++ logic.js
@@ -0,0 +1,22 @@
+let likebtn = document.querySelector(".likebtn");
+let likecount= document.querySelector(".like-count");
+let commentbtn = document.querySelector(".commentbtn");
+let commentcount = document.querySelector(".comment-count");
+let sharebtn = document.querySelector(".sharebtn");
+let sharecount = document.querySelector(".share-count");
+
+let count1 = 0 ;
+likebtn.addEventListener("click", () => {
+    count1++;
+    likecount.innerText = count1;
+})
+let count2 = 0;
+commentbtn.addEventListener("click", () => {
+    count2++;
+    commentcount.innerText = count2;
+})
+let count3 = 0 ;
+sharebtn.addEventListener("click", () => {
+    count3++;
+    sharecount.innerText = count3;
+})
